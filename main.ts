f02f7dc516d098aff470167371ff15241103f938
Kopi-Kopsen <posnerla@thg-heilbronn.de>
2024-11-21 10:53:33 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    radio.sendString("K")
+})
+// Setzt die Funkgruppe auf 1
+input.onButtonPressed(Button.A, function () {
+    // Sende Befehl für "Links"
+    radio.sendString("L")
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("S")
+})
+input.onButtonPressed(Button.AB, function () {
+    // Sende Befehl für "Stop"
+    radio.sendString("F")
+})
+input.onButtonPressed(Button.B, function () {
+    // Sende Befehl für "Rechts"
+    radio.sendString("R")
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    radio.sendString("B")
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    radio.sendString("K")
+})
+// Setzt die Funkgruppe auf 1
+radio.setGroup(2)
+radio.setTransmitPower(7)
